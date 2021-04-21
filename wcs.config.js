65cacf2920bf22df021514a414324583c4f7bc64
omerken <48179317+omerken@users.noreply.github.com>
2021-04-21 18:46:00 +0300

    Add Init-Scripts to wcs-example-project (#5)
    * Add Init-Scripts to wcs-example-project
    
    * replaced installation command to `npm i`

--- wcs.config.js
+++ wcs.config.js
@@ -2,4 +2,11 @@ module.exports = {
     previewTypeConfiguration: {
         previewType: 'compiled',
     },
+    scripts: {
+        install: {
+            title: 'Install',
+            description: 'Run install',
+            command: 'npm i',
+        }
+    },
 }
