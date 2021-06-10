ef003142fa10b4d6615559c1684d48bb99b090b8
mikawix <mikan@wix.com>
2021-06-10 11:34:21 +0300

    blank component

--- _wcs/simulations/AppProductItem/product-added-to-cart.sim.tsx
+++ _wcs/simulations/AppProductItem/product-added-to-cart.sim.tsx
@@ -5,19 +5,12 @@ import { CommonSimulationsSetup } from '../../setup/common-simulations-setup';
 export default createSimulation({
     componentType: AppProductItem,
     props: {
-        productId: 'grw2as',
-        imageUrl:
-            '',
-        productTitle: 'Product Title',
-        modelName: 'select model',
-        price: '$0.00',
-        isAddedToCart: false,
-        isNew: false
+       
     },
     setup: [...CommonSimulationsSetup],
     environmentProps: {
         windowHeight: 726,
         canvasWidth: 360,
     },
-    name: 'Product Added To Cart',
+    name: 'Product Card',
 });
--- src/components/app-product-item/app-product-item.tsx
+++ src/components/app-product-item/app-product-item.tsx
@@ -1,106 +1,17 @@
 import React from 'react';
 import { classes, style } from './app-product-item.st.css';
-import { StarsRating } from '../stars-rating/stars-rating';
 import { FlexLayout } from '../../common-classes/flex-layout/flex-layout';
-import { ImageView } from '../image-view/image-view';
 import { CommonClasses } from '../../common-classes/common-classes/common-classes';
 
-export interface AppProductItemProps {
-    imageUrl: string;
-    modelName?: string;
-    productId: string;
-    productTitle: string;
-    price: string;
-    isNew?: boolean;
 
-    reviewsAverageRating?: number;
-    reviewsCount?: number;
+export const AppProductItem = React.memo(() => {
 
-    onAddToCartButtonClick?: (productId: string) => void;
-    isAddedToCart?: boolean;
 
-    className?: string;
-}
 
-export const AppProductItem = React.memo<AppProductItemProps>((props) => {
-    const {
-        productId,
-        productTitle,
-        modelName,
-        imageUrl,
-        reviewsCount,
-        reviewsAverageRating,
-        onAddToCartButtonClick,
-        isAddedToCart = false,
-        price,
-        isNew = false,
-
-        className,
-        ...htmlAttributes
-    } = props;
-
-    const normalizedRating =
-        reviewsAverageRating !== undefined
-            ? Math.min(Math.max(reviewsAverageRating, 0), 5)
-            : undefined;
-
-    const productUrl = `/product/${productId}`;
 
     return (
         <div
-            {...htmlAttributes}
-            className={style(classes.root, FlexLayout.column.default, className)}
-        >
-            <div className={style(classes.sectionMain, CommonClasses.positioned)}>
-                <a href={productUrl} className={CommonClasses.resetDefaultStyle}></a>
-                <div
-                    className={style(
-                        classes.addToCartButton,
-                        { isAddedToCart: isAddedToCart },
-                        CommonClasses.button,
-                        FlexLayout.centerContent
-                    )}
-                    onClick={() => {
-                        if (onAddToCartButtonClick) {
-                            onAddToCartButtonClick(productId);
-                        }
-                    }}
-                >
-                    {isAddedToCart ? 'Remove From Cart' : 'Add To Cart'}
-                </div>
-                {isNew ? (
-                    <div className={style(classes.bannerNew, FlexLayout.centerContent)}>New</div>
-                ) : null}
-            </div>
-            <div className={FlexLayout.row.alignToStart}>
-                <div className={FlexLayout.fillRemainingHorizontalSpace}>
-                    <div className={style(classes.title, CommonClasses.button)}>
-                        <a href={productUrl} className={CommonClasses.resetDefaultStyle}>
-                            {productTitle}
-                        </a>
-                    </div>
-                    <div className={style(classes.modelName)}>{modelName}</div>
-                </div>
-                <div className={style(FlexLayout.column.alignToEnd)}>
-                    <div className={style(classes.priceLabel)}>{price}</div>
-                    {normalizedRating !== undefined ? (
-                        <div className={style(classes.rating)}>
-                            <StarsRating
-                                className={style(classes.starsRating)}
-                                rating={normalizedRating}
-                            />
-                            {reviewsCount !== undefined ? (
-                                <div className={style(classes.reviewsCount, {})}>
-                                    ({reviewsCount} reviews)
-                                </div>
-                            ) : null}
-                        </div>
-                    ) : null}
-                </div>
-            </div>
-            <div
-                className={style(classes.sectionModelNameAndRating, FlexLayout.row.alignToStart)}
-            ></div>
-        </div>
+            className={style(classes.root, FlexLayout.column.default)}
+        ></div>
     );
 });
