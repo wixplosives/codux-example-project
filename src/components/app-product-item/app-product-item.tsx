7af9a3102a82f8dacb4df63803a9a2ce1c6a5e78
mikawix <mikan@wix.com>
2021-06-16 09:05:21 +0300

    New component

--- src/components/app-product-item/app-product-item.tsx
+++ src/components/app-product-item/app-product-item.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { classes, style } from './app-product-item.st.css';
 import { FlexLayout } from '../../common-classes/flex-layout/flex-layout';
 import { CommonClasses } from '../../common-classes/common-classes/common-classes';
+import { ImageView } from '../image-view/image-view';
+import { StarsRating } from '../stars-rating/stars-rating';
+import { classes as classes0 } from '../../common-classes/flex-layout/flex-layout.st.css';
 
 
 export const AppProductItem = React.memo(() => {
 
 
-
-
     return (
         <div
-            className={style(classes.root, FlexLayout.column.default)}
+            className={classes.root}
         ></div>
     );
 });
