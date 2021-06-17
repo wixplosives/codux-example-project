75c806314602e09bdf952c5adc676e1e37a44f5c
Jonathan Marton <jonathan.marton@gmail.com>
2021-06-17 16:10:56 +0300

    additional fix

--- _wcs/simulations/BreadCrumbs/bread-crumbs-multiple-items.sim.tsx
+++ _wcs/simulations/BreadCrumbs/bread-crumbs-multiple-items.sim.tsx
@@ -1,7 +1,6 @@
 import { createSimulation } from '@wixc3/wcs-core';
 import { BreadCrumbs } from '../../../src/components/bread-crumbs/bread-crumbs';
 import { CommonSimulationsSetup } from '../../setup/common-simulations-setup';
-import { SiteMap } from '../../../src/stores/site-map';
 
 export default createSimulation({
     componentType: BreadCrumbs,
