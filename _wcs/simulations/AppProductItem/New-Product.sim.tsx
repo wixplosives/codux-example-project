f2eb35e2bec8adf20fdd21dc4e6c03a8eaf8fa28
Jonathan Marton <jonathan.marton@gmail.com>
2021-06-17 17:55:01 +0300

    additional changes to branch

--- _wcs/simulations/AppProductItem/New-Product.sim.tsx
+++ _wcs/simulations/AppProductItem/New-Product.sim.tsx
@@ -8,17 +8,18 @@ export default createSimulation({
         productId: 'grw2as',
         imageUrl:
             'https://static.wixstatic.com/media/d759b6_d1773d716a4746d8b70eb7c15112000e~mv2.png',
-        productTitle: 'Swim Swim Swim',
+        productTitle: 'Swim',
         modelName: 'Red on Cyan',
         price: '$9.00',
         reviewsAverageRating: 4,
         reviewsCount: 22,
         isAddedToCart: false,
+        isNew: true
     },
     setup: [...CommonSimulationsSetup],
     environmentProps: {
         windowHeight: 726,
         canvasWidth: 360,
     },
-    name: 'Product With Long Title',
+    name: 'New Product',
 });
