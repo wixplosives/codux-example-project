f179fa687eda10a173b6daad521918ccb187ca2e
mikawix <mikan@wix.com>
2021-06-28 17:49:13 +0300

    link to product

--- src/components/app-product-item/app-product-item.tsx
+++ src/components/app-product-item/app-product-item.tsx
@@ -5,12 +5,10 @@ import { classes } from './app-product-item.st.css';
 import { ImageView } from '../image-view/image-view';
 import { classes as classes0, style } from '../../common-classes/flex-layout/flex-layout.st.css';
 import { StarsRating } from '../stars-rating/stars-rating';
+import { classes as classes1 } from '../stars-rating/stars-rating.st.css';
 
 
 export const AppProductItem = React.memo(() => {
-
-
-
 
     return (
         <div
@@ -20,7 +18,8 @@ export const AppProductItem = React.memo(() => {
                 <ImageView resizeMode={'contain'} positionVertical={'center'} positionHorizontal={'center'} src={'https://cdn.shopify.com/s/files/1/2170/8465/products/STEVEMADDEN-SHOES_RHODE_BLACK-LEATHER_600x.jpg?v=1616454947'} className={classes.productImage} />
             </div>
             <div className={style(classes0.flexColumnAlignToStart, classes.footer)}>
-                <h1 className={classes.productTitle}>RHODE BLACK LEATHER</h1>
+                <a href="/" className={classes.productLink}>
+                    <h1 className={classes.productTitle}>RHODE BLACK LEATHER</h1></a>
                 <div className={classes0.flexRowAlignToStart}>
                     <h1 className={classes.productOrigPrice}>$84.95</h1>
                     <h1 className={classes.productPrice}>$59.99</h1>
