ddc5194d06c1e3b2d6a6b611fdebf3b699d49e28
mikawix <mikan@wix.com>
2021-08-25 10:43:27 +0300

    addToCart

--- src/components/app-product-item/app-product-item.tsx
+++ src/components/app-product-item/app-product-item.tsx
@@ -17,16 +17,19 @@ export const AppProductItem = React.memo(() => {
             <div className={style(classes0.flexColumnAlignToCenter, classes.main)}>
                 <ImageView resizeMode={'contain'} positionVertical={'center'} positionHorizontal={'center'} src={'https://cdn.shopify.com/s/files/1/2170/8465/products/STEVEMADDEN-SHOES_RHODE_BLACK-LEATHER_600x.jpg?v=1616454947'} className={classes.productImage} />
             </div>
-            <div className={style(classes0.flexColumnAlignToStart, classes.footer)}>
-                <a href="/" className={classes.productLink}>
-                    <h1 className={classes.productTitle}>RHODE BLACK LEATHER</h1>
-                    <h3>Product description</h3>
-                </a>
-                <div className={classes0.flexRowAlignToStart}>
-                    <h1 className={classes.productOrigPrice}>$84.95</h1>
-                    <h1 className={classes.productPrice}>$59.99</h1>
+            <div className={classes.addToCartButton}>Add To Cart</div>
+            <div className={style(classes0.flexRowStretch, classes0.footer)}>
+
+                <div className={style(classes0.flexColumnAlignToStart)}>
+                    <a href="/" className={classes.productLink}>
+                        <h1 className={classes.productTitle}>RHODE BLACK LEATHER</h1></a>
                 </div>
-                <StarsRating rating={5} className={classes.starsRating} /></div>
+                <div className={classes0.flexColumnAlignToEnd}>
+                    <div className={classes0.flexRowAlignToStart}>
+                        <h1 className={classes.productOrigPrice}>$84.95</h1>
+                        <h1 className={classes.productPrice}>$59.99</h1>
+                    </div>
+                    <StarsRating rating={3} className={classes.starsRating} /></div></div>
         </div>
     );
 });
