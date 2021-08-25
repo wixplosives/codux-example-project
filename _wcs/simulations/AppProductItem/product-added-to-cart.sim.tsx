ce8cfeb52e2e000572879e4f3fbddc722ddbe0ae
mikawix <mikan@wix.com>
2021-08-25 15:51:27 +0300

    .

--- _wcs/simulations/AppProductItem/product-added-to-cart.sim.tsx
+++ _wcs/simulations/AppProductItem/product-added-to-cart.sim.tsx
@@ -5,13 +5,17 @@ import { CommonSimulationsSetup } from '../../setup/common-simulations-setup';
 export default createSimulation({
     componentType: AppProductItem,
     props: {
+        currentPrice: '$59.99',
+        originalPrice: '$84.95',
+        productTitle: 'Product Title',
+        isAddedToCart: false
 
     },
     setup: [...CommonSimulationsSetup],
     environmentProps: {
         windowHeight: 812,
-        canvasWidth: 360,
-        windowWidth: 375,
+        canvasWidth: 388,
+        windowWidth: 367,
         canvasHeight: 455
     },
     name: 'Product Card',
