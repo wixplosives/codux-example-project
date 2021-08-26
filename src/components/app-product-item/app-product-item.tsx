f2076597990ba76dbdc8778734eeb91b073da266
mikawix <mikan@wix.com>
2021-08-26 11:56:34 +0300

    scarve

--- src/components/app-product-item/app-product-item.tsx
+++ src/components/app-product-item/app-product-item.tsx
@@ -8,24 +8,24 @@ import { StarsRating } from '../stars-rating/stars-rating';
 import { classes as classes1 } from '../stars-rating/stars-rating.st.css';
 
 export interface AppProductItemProps {
-   productTitle: string;
-   /** Product cost */
-   originalPrice: string;
-currentPrice: string;
-   /** Toggles the 'new' button on and off */
-   isAddedToCart?: boolean;
-   }
+    productTitle: string;
+    /** Product cost */
+    originalPrice: string;
+    currentPrice: string;
+    /** Toggles the 'new' button on and off */
+    isAddedToCart?: boolean;
+}
 
 
 export const AppProductItem = React.memo<AppProductItemProps>((props) => {
-   const {
-       
-       productTitle,
-       isAddedToCart = false,
-       originalPrice,
-	   currentPrice,
-       ...htmlAttributes
-   } = props;
+    const {
+
+        productTitle,
+        isAddedToCart = false,
+        originalPrice,
+        currentPrice,
+        ...htmlAttributes
+    } = props;
 
 
     return (
@@ -33,7 +33,7 @@ export const AppProductItem = React.memo<AppProductItemProps>((props) => {
             className={classes.root}
         >
             <div className={style(classes0.flexColumnAlignToCenter, classes.main)}>
-                <ImageView resizeMode={'contain'} positionVertical={'center'} positionHorizontal={'center'} src={'https://cdn.shopify.com/s/files/1/2170/8465/products/STEVEMADDEN-SHOES_RHODE_BLACK-LEATHER_600x.jpg?v=1616454947'} className={classes.productImage} />
+                <ImageView resizeMode={'contain'} positionVertical={'center'} positionHorizontal={'center'} src={'https://static.wixstatic.com/media/1f3c0f_62b49fd6f9bb40be878c33551049c10d.jpg/v1/fill/w_500,h_500,al_c,q_85,usm_0.66_1.00_0.01/1f3c0f_62b49fd6f9bb40be878c33551049c10d.webp'} className={classes.productImage} />
             </div>
             <div className={style0(classes.addToCartButton, classes0.flexCenterContent)}>Add To Cart</div>
             <div className={style(classes0.flexRowStretch, classes0.footer)}>
