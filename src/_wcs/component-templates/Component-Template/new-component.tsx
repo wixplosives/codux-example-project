80d8f7ba6dd10db2c25541eba6b8bc23457a48b9
Omer <omerk@wix.com>
2022-02-05 16:24:44 +0200

    new component template

---
+++ src/_wcs/component-templates/Component-Template/new-component.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './new-component.st.css';
+
+export const NewComponent = () => (
+    <div className="root">I am a NewComponent</div>
+);
--- wcs.config.js
+++ wcs.config.js
@@ -9,5 +9,6 @@ module.exports = {
   },
   newComponent: {
     componentsPath: 'src/components',
+    templatesPath:  'src/_wcs/component-templates',
   },
 };
