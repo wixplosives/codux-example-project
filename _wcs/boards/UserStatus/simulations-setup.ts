d2d4f9a3dd366548db0a4f8c264ac45ced2f3114
Ari Kishon <ariki@wix.com>
2022-02-13 17:34:36 +0200

    chore: remaining instances of simulations

--- _wcs/boards/UserStatus/simulations-setup.ts
+++ _wcs/boards/UserStatus/simulations-setup.ts
@@ -3,7 +3,7 @@ import { createGoogleFontLoader } from '../../setup/google-font-loader';
 export const UserStatusSimulationsSetup = [
     /**
      * The UserInfo component uses the 'Prompt' font.
-     * Therefore, we'll load it in all the component's simulations.
+     * Therefore, we'll load it in all the component's Boards.
      */
     createGoogleFontLoader('Prompt', ['300', '400', '500']),
 ];
