b22fdb7b1e515e45541fe9b4e7b547db4476b575
Ari Kishon <37385467+Ari-Kishon@users.noreply.github.com>
2022-02-14 09:21:16 +0200

    fix: change casing of one word in comment

--- _wcs/boards/UserStatus/simulations-setup.ts
+++ _wcs/boards/UserStatus/simulations-setup.ts
@@ -3,7 +3,7 @@ import { createGoogleFontLoader } from '../../setup/google-font-loader';
 export const UserStatusSimulationsSetup = [
     /**
      * The UserInfo component uses the 'Prompt' font.
-     * Therefore, we'll load it in all the component's Boards.
+     * Therefore, we'll load it in all the component's boards.
      */
     createGoogleFontLoader('Prompt', ['300', '400', '500']),
 ];
